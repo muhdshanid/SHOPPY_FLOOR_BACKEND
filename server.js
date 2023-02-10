9e95bae77ba201211a2cf602064623aed0848993
Muhammed Shanid <shanishanid91@gmail.com>
2023-02-10 22:34:20 +0530

    initial commit

--- server.js
+++ server.js
@@ -19,10 +19,14 @@ import orderRouter from './routes/orderRoutes.js'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
-
+const corsOptions ={
+    origin:'', 
+    credentials:true, 
+    optionSuccessStatus:200
+}
 connectDB()
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors(corsOptions))
 app.get('/api/keys/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
   });
@@ -55,3 +59,26 @@ app.use(errorHandler)
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
 })
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
